81f32aea2d37ef0d96577bba5d8dc7f553e6b42d
sujin.choi <amor77777777778@gmail.com>
2025-01-03 17:20:27 +0900

    헤더 내용과 스타일 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,55 +21,61 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50">
-      <div className="h-6"></div>
-      <div className="mx-auto max-w-screen-xl px-4">
-        <div className="mx-auto max-w-[1000px] bg-background rounded-2xl border overflow-hidden shadow-sm dark:shadow-gray-700/50">
-          <div className="flex h-14 items-center justify-between px-3">
-            <Link
-              href="/"
-              className="ml-2 flex items-center text-lg font-bold text-gray-700 dark:text-gray-300"
-              aria-label="홈으로 이동"
-            >
-              <span className="text-primary">leerowoon</span>.dev
-            </Link>
-            <nav>
-              <ul className="flex items-center gap-1">
-                {navigation.map((item) => (
-                  <li key={item.name}>
-                    <Link
-                      href={item.href}
-                      className={cn(
-                        "px-3 py-2 text-sm transition-colors",
-                        pathname === item.href
-                          ? "text-black dark:text-white"
-                          : "text-gray-500 hover:text-black dark:text-gray-400 dark:hover:text-white"
-                      )}
-                      aria-label={`${item.name} 페이지로 이동`}
-                    >
-                      {item.name}
-                    </Link>
-                  </li>
-                ))}
-                <li>
-                  <button
-                    onClick={handleThemeToggle}
-                    className="ml-2 rounded-md p-2 text-gray-600 hover:bg-gray-100/50 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-800/50 dark:hover:text-white"
-                    aria-label={
-                      theme === "dark"
-                        ? "라이트 모드로 전환"
-                        : "다크 모드로 전환"
-                    }
-                  >
-                    {theme === "dark" ? (
-                      <FiSun onClick={handleThemeToggle} />
-                    ) : (
-                      <FiMoon onClick={handleThemeToggle} />
+      <div className="max-w-full w-full bg-background border-b overflow-hidden shadow-sm dark:shadow-gray-800/50">
+        <div className="flex h-14 items-center justify-between px-3">
+          <Link
+            href="/"
+            className="ml-2 flex items-center text-lg font-bold"
+            aria-label="홈으로 이동"
+          >
+            <span>🌙🌿🌷💤</span>
+            {/* <span className="relative inline-block overflow-hidden group">
+              <p className="inline-block transition-transform duration-300 group-hover:-translate-y-full">
+                
+              </p>
+              <p className="absolute left-0 inline-block translate-y-full transition-transform duration-300 group-hover:translate-y-0">
+                <span className="text-primary font-bold text-lg">
+                  While True,
+                </span>
+                <span className="text-foreground ml-1">Sleep</span>
+              </p>
+            </span> */}
+          </Link>
+          <nav>
+            <ul className="flex items-center gap-1">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  <Link
+                    href={item.href}
+                    className={cn(
+                      "px-3 py-2 text-sm transition-colors",
+                      pathname === item.href
+                        ? "text-primary-foreground bg-primary/20 dark:bg-primary/30"
+                        : "hover:text-primary dark:hover:text-primary-foreground"
                     )}
-                  </button>
+                    aria-label={`${item.name} 페이지로 이동`}
+                  >
+                    {item.name}
+                  </Link>
                 </li>
-              </ul>
-            </nav>
-          </div>
+              ))}
+              <li>
+                <button
+                  onClick={handleThemeToggle}
+                  className="ml-2 rounded-md p-2 transition-colors text-muted-foreground hover:bg-muted/80 dark:hover:bg-muted/60 dark:text-muted-foreground"
+                  aria-label={
+                    theme === "dark" ? "라이트 모드로 전환" : "다크 모드로 전환"
+                  }
+                >
+                  {theme === "dark" ? (
+                    <FiSun className="text-warning" />
+                  ) : (
+                    <FiMoon className="text-primary" />
+                  )}
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
