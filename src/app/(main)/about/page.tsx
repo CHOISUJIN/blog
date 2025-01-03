d0c40b867421a009d623b9db9f36770d516f3695
sujin.choi <amor77777777778@gmail.com>
2025-01-03 17:24:20 +0900

    About 페이지의 정보 수정

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import SkillText from "@/components/about/skill-text";
 import IconWrapper from "@/components/about/icon-wrapper";
 import { baseUrl, siteName, siteMetadata } from "@/lib/metadata";
-import { FixedSizeImgWithPlaceholder } from "@/components/fixed-size-img-with-placeholder";
 
 export async function generateMetadata(): Promise<Metadata> {
   const pageTitle = "About";
@@ -35,20 +34,18 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 const SKILLS = [
-  "React Native",
-  "iOS",
-  "Android",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Nest.js",
-  "Docker",
-  "Git",
-  "앱 취약성 점검",
+  "ASP.NET",
+  "Vue",
+  "SQL",
+  "JavaScript",
+  "HTML",
+  "CSS",
   "TypeScript",
-  "Java",
-  "Swift",
-  "Objective-C",
+  "REST API",
+  "Git",
+  "Entity Framework",
+  "C#",
+  "LINQ",
   "...",
 ];
 
@@ -56,25 +53,20 @@ export default function AboutPage() {
   return (
     <div className="container mx-auto max-w-[750px] mt-36 mb-16 flex flex-col gap-14 p-2 px-6">
       <section className="flex flex-col justify-center items-center gap-4">
-        <FixedSizeImgWithPlaceholder
-          className="rounded-full size-28"
-          src="/images/profile_square.jpg"
-          alt="profile"
-          width={112}
-          height={112}
-        />
-        <h1 className="text-3xl font-bold">이로운</h1>
-        <p className="text-sm text-center">
-          Software Engineer | Mobile, React Native Developer
-        </p>
+        <div className="w-36 h-36 rounded-full bg-primary/20 dark:bg-primary/30 flex items-center justify-center">
+          {/* <span className="text-gray-700"> 🐨 </span> */}
+        </div>
+
+        <h1 className="text-3xl font-bold">최수진</h1>
+        <p className="text-sm text-center">Software Engineer | Web Developer</p>
         <div className="flex gap-3">
-          <Link href="mailto:2circumflex@gmail.com">
+          <Link href="mailto:amor77777777778@gmail.com">
             <IconWrapper>
               <MdEmail size={14} />
             </IconWrapper>
           </Link>
           <Link
-            href="https://github.com/2circumflex"
+            href="https://github.com/CHOISUJIN"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -83,7 +75,7 @@ export default function AboutPage() {
             </IconWrapper>
           </Link>
           <Link
-            href="https://www.linkedin.com/in/2circumflex/"
+            href="https://www.linkedin.com/in/sooooooo/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -96,34 +88,8 @@ export default function AboutPage() {
       <section className="flex flex-col gap-5">
         <h2 className="text-2xl font-bold">소개</h2>
         <p className="leading-7 text-muted-foreground">
-          안녕하세요, 이로운입니다. 👋
-          <br />
-          모바일 앱 개발에 주력해왔으며, 현재는 React Native를 활용한 앱 개발에
-          집중하고 있습니다.
-          <br />
-          하지만 특정 기술에만 국한되지 않고, 필요한 기술이라면 언제든 배우고
-          활용하며 성장합니다.
+          안녕하세요, 최수진입니다. 🐨
         </p>
-        <div className="flex flex-col gap-2">
-          <h3 className="text-md font-medium underline underline-offset-4">
-            저는 책임감이 있습니다.
-          </h3>
-          <p className="leading-7 text-muted-foreground">
-            문제가 발생했을 때 회피하지 않고 끝까지 해결하려는 태도로 임합니다.
-            맡은 일에 대해 책임감을 가지고 끝까지 완수하며, 주니어 시절부터
-            지금까지 모든 프로젝트를 성공적으로 마무리해온 경험이 있습니다.
-          </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h3 className="text-md font-medium underline underline-offset-4">
-            협업과 의사소통을 중요하게 생각합니다.
-          </h3>
-          <p className="leading-7 text-muted-foreground">
-            단순히 주어진 업무를 수행하는 데 그치지 않고, 능동적이고 열린 자세로
-            팀원들과 협력합니다. 팀의 목표와 비전을 달성하기 위해 적극적으로
-            의사소통하고 함께 해결책을 찾아갑니다.
-          </p>
-        </div>
       </section>
       <section className="flex flex-col gap-5">
         <h2 className="text-2xl font-bold">스킬</h2>
--- src/components/about/icon-wrapper.tsx
+++ src/components/about/icon-wrapper.tsx
@@ -4,7 +4,7 @@ export default function IconWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className="p-2 rounded-md border dark:border-gray-700/80 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-200">
+    <div className="p-2 rounded-md hover:bg-primary/20 dark:hover:bg-primary/30 transition-colors duration-200">
       {children}
     </div>
   );
