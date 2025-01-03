147f1fc3eaed58a611e83183f05996725ad0ea79
sujin.choi <amor77777777778@gmail.com>
2025-01-03 17:26:20 +0900

    메타데이터 수정

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,13 +1,9 @@
 import { Metadata } from "next";
 
-export const siteName = "이로운 개발";
-export const title = `이로운 개발`;
+export const siteName = "최수진 블로그";
+export const title = `최수진 블로그`;
 export const description = "개발 관련된 이야기를 합니다.";
-export const keywords = [
-  "이로운 개발자",
-  "개발자 블로그",
-  "소프트웨어 엔지니어",
-];
+export const keywords = ["개발자", "개발자 블로그", "소프트웨어 엔지니어"];
 export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const siteMetadata: Metadata = {
