bf4f6e3334003a241ec52c02ad5c929a3194fdd2
sujin.choi <amor77777777778@gmail.com>
2025-01-04 18:25:29 +0900

    카테고리 레이아웃 추가하기

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -24,8 +24,22 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function Home() {
+
+  const categories = [{ name: '🔖 전체', count: 10 }, { name: '💻 개발', count: 6 }, { name: '🏖️ 여행', count: 3 }, { name: '💰 투자', count: 1 }];
+
   return (
     <div className="container mx-auto max-w-[900px] mt-32 mb-16 flex flex-col p-2 px-6">
+      <div className="mb-10">
+        <ul className="flex gap-2">
+          {categories.map((item, index) => (
+            <li key={index}>
+              <button className="rounded-full px-4 py-2 bg-primary hover:bg-accent focus:bg-accent focus:ring-4 focus:ring-accent font-semibold shadow-lg transition-all duration-200 dark:bg-secondary dark:hover:bg-accent dark:focus:bg-accent dark:focus:ring-4 dark:focus:ring-accent ">
+                {item.name} ({item.count})
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
       <RecentPost />
     </div>
   );
