90a27958d6a31a46c6ed7deb61a9023e0ab02fc9
sujin.choi <amor77777777778@gmail.com>
2025-01-10 16:52:13 +0900

    게시글 추가하기

--- src/components/post-detail/post-header.tsx
+++ src/components/post-detail/post-header.tsx
@@ -22,7 +22,7 @@ export default function PostHeader({ post }: { post: Post }) {
           {dayjs(post.date).locale("ko").format("YYYY년 MM월 DD일")}
         </p>
       </div>
-      <div className="mt-4 mb-0 flex flex-wrap gap-2">
+      <div className="mt-4 mb-4 flex flex-wrap gap-2">
         {post.tags.map((tag) => (
           <Link href={`/tags/${tag}`} key={tag} className="no-underline">
             <p className="mt-0 mb-0 text-sm bg-gray-100 text-gray-500 dark:text-gray-700 dark:bg-gray-300 rounded-full px-2.5 py-2 hover:bg-gray-200 dark:hover:bg-gray-400 transition-colors">
